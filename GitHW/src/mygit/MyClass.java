8ae369e01446c3aedc489450a59a8eb30c6281cd
p1001q <p1001q@gmail.com>
2025-12-03 14:10:56 +0900

    Add: Change in master branch

--- GitHW/src/mygit/MyClass.java
+++ GitHW/src/mygit/MyClass.java
@@ -4,5 +4,6 @@ public class MyClass {
 	public static void main(String[] args) {
 	System.out.println("Hello Git!");
 	System.out.println("This is my second version.");
+	System.out.println("My changed in master branch");
 	}
 }
